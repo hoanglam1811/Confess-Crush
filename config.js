5945d064b0e860312e8fcece92844affe0e4c6c8
hoanglam1811 <100185135+hoanglam1811@users.noreply.github.com>
2022-02-22 12:50:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "nho chi qa dm",
+    introTitle: 'tdan bi khung',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
