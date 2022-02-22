e756d551421d7faad54dada374a1d995dd675a45
hoanglam1811 <100185135+hoanglam1811@users.noreply.github.com>
2022-02-22 13:24:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "nhin lam cho' =))",
-    introTitle: 'tdan bi dien a???',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'em ghet chi =))))',
+    introDesc: `nhẹ nhàng mình bên nhau
+    nhẹ nhàng cầm tay nhau..`,
     btnIntro: 'bà lúc nào cũng xàm hơn tui =)))',
-    title: 'đang hoc à =)))',
+    title: 'đang học à =)))',
     desc: 'hoc ma van bam tiep???',
     btnYes: 'tai nho em =))))',
     btnNo: 'hong nho em =)))',
-    question: 'vô được tới đây là iu em rồi =)))',
+    question: 'co bi khung ko =)))',
     btnReply: 'gửi cho bạn <3',
     reply: 'iu thì iu mà không yêu thì yêu <33333333',
-    mess: 'biết mà =)))). mình cũng iu b nhiều =)))',
+    mess: 'biết mà =))) mình cũng iu b nhiều =)))',
     messDesc: 'toi toi em hoc xong em mua gi cho an nhaaa',
     btnAccept: 'ò oke <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/congchuabuoito123
+    messLink: 'https://www.facebook.com/congchuabuoito123'
 }
